275d3be3016c5f84f6e2d73e91869521c4db22b4
Lee O'Mara <lee.omara@gov.yk.ca>
2022-12-10 21:58:46 -0700

    Parse out entity info

--- ycor_scraper/ycor_scraper/spiders/entity_spider.py
+++ ycor_scraper/ycor_scraper/spiders/entity_spider.py
@@ -17,3 +17,14 @@ class Entitypider(scrapy.Spider):
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {filename}')
+        for entity in response.css("table tbody tr"):
+            yield {
+                'name': entity.css('td.name-view span span::text').get(),
+                # Most don't have a former name here, but some do.
+                'former_name': entity.css('td.name-view').re_first(r'<b>Former Name:<\/b\>\xa0(.*)'),
+                'entity_type': entity.css('span.entity-entity-type-translated::text').get(),
+                'registry_number': entity.css('span.entity-fileno::text').get(),
+                'jurisdiction': entity.css('span.entity-jurisdiction::text').get(),
+                'status': entity.css('span.entity-status-translated::text').get()
+                # Filing and Profile reports are URLs that can be generated based on registry number.
+            }
